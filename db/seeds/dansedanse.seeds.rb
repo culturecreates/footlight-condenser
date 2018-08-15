4980579eaa891c343bd49c1909f917f20d62289d
Gregory Saumier-Finch <saumierfinch@gmail.com>
2018-08-15 08:47:56 -0400

    added rake task to seed a single website

---
+++ db/seeds/dansedanse.seeds.rb
@@ -0,0 +1,40 @@
+# rails db:seed:single SEED=dansedanse
+
+@site = Website.create!(seedurl: "dansedanse-ca")
+
+# ["URL","URI","en"], ["URL","URI","fr"]
+pages = [
+
+]
+
+
+pages.each do |page|
+  Webpage.create!(url: page[0], rdf_uri: page[1], language: page[2], website: @site, rdfs_class: RdfsClass.where(name: "Event").first)
+end
+
+
+def self.create_source( label, algo, next_algo = "", selected = true, languages = [""])
+
+  languages.each do |lang|
+    if !next_algo.blank?
+      s = Source.create!(language: lang, render_js: true, website: @site, property: Property.where(label: label).first, algorithm_value:next_algo, selected:selected)
+      Source.create!(language: lang, next_step: s.id, website: @site, property: Property.where(label: label).first, algorithm_value:algo, selected:selected)
+    else
+      Source.create!(language: lang, website: @site, property: Property.where(label: label).first, algorithm_value:algo, selected:selected)
+    end
+  end
+end
+
+create_source("Title","","",true,["en","fr"])
+create_source("Description","","",false,["en","fr"])
+create_source("Description","","",true,["en","fr"])
+create_source("Webpage link","","",true,["en","fr"])
+create_source("Organized by","")
+create_source("Produced by","manual=Enter the organization that produced this event")
+create_source("Performed by","manual=Enter the organization that performed this event")
+create_source("Tickets link","","",true,["en","fr"])
+create_source("Photo","")
+
+create_source("Date","")
+create_source("Time","")
+create_source("Location","")
