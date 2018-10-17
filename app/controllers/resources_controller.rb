b88c35c0aaa24b3206e1598f430dd9d4b3424786
Gregory Saumier-Finch <saumierfinch@gmail.com>
2018-10-17 17:40:10 -0400

    added ability to detect next event that needs review

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -39,6 +39,7 @@ class ResourcesController < ApplicationController
 
   # PATCH /resources/:rdf_uri/reviewed_all
   def reviewed_all
+
     @statements = []
     _webpages = Webpage.where(rdf_uri: params[:rdf_uri])
     _webpages.each do |webpage|
@@ -47,11 +48,20 @@ class ResourcesController < ApplicationController
       end
     end
     @statements.each do |statement|
-      statement.update!(status: "ok", status_origin: params[:event][:status_origin]) if (statement.source.selected && !statement.is_problem? && !statement.is_missing?)
+      statement.update!(status: "ok", status_origin: params[:event][:status_origin]) if (statement.source.selected && !statement.is_problem?)
+    end
+
+    uri_to_load = params[:rdf_uri]
+    if params[:review_next] == "true"
+      #get next rdf_uri
+      uris_to_review = Statement.joins({webpage: :website},:source, {webpage: :rdfs_class}).where(webpages:{websites: {seedurl: params[:seedurl]}, rdfs_classes: {name: "Event"}}).where(sources: {selected: true}).where(status: "initial").pluck(:rdf_uri).uniq
+      if !uris_to_review.blank?
+        uri_to_load = uris_to_review.first
+      end
     end
     respond_to do |format|
-      format.html { redirect_to show_resources_path(rdf_uri: params[:rdf_uri], format: :html) }
-      format.json { redirect_to show_resources_path(rdf_uri: params[:rdf_uri], format: :json)}
+      format.html { redirect_to show_resources_path(rdf_uri: uri_to_load, format: :html) }
+      format.json { redirect_to show_resources_path(rdf_uri: uri_to_load, format: :json)}
     end
   end
 
