c5b1329caf3afeb0149ccc53ee621b4387ff1923
Gregory Saumier-Finch <saumierfinch@gmail.com>
2018-10-27 16:56:23 -0400

    added json-ld processing for co-motion.ca and laval.footlight.io

--- app/controllers/structured_data_controller.rb
+++ app/controllers/structured_data_controller.rb
@@ -1,5 +1,10 @@
 class StructuredDataController < ApplicationController
 
+
+  def place_markup
+    #TODO: add route and code to create JSON-LD for Places
+  end
+
   # GET /structured_data/event_markup?url=
   def event_markup
     params[:url]
@@ -21,53 +26,32 @@ class StructuredDataController < ApplicationController
           {
             "@vocab": "http://schema.org",
             "description_en":{"@id": "description", "@language": "en"},
-            "description_fr":{ "@id": "description","@language": "fr"},
+            "description_fr":{"@id": "description","@language": "fr"},
             "name_en":{"@id": "name",	"@language": "en"},
       		  "name_fr":{"@id": "name", "@language": "fr"}
           },
         "@type": "Event",
-        "superEvent": "#{webpage.rdf_uri}"
+        "superEvent": { "@id": "#{webpage.rdf_uri}"}
         }
-
 
       _condensor_statements.each do |statement|
         if statement.source.selected == true  && (statement.status == "ok" || statement.status == "updated")  && (statement.source.language == _lang || statement.source.language == "")
           prop = statement.source.property.uri.to_s.split("/").last
-
           if prop != nil
             if statement.source.property.value_datatype == "xsd:anyURI"
-              begin
-                _uri_statement = statement.cache
-                # Handle 2 possible data structres by making both into a list of arrays.
-                #1: [["source 1","Place",["place name","adr:palce_uri"]],[]]
-                #2: ["source 1","Place",["place name","adr:palce_uri"]]
-                if !_uri_statement.starts_with?("[[")
-                  _uri_statement = "[#{_uri_statement}]"
-                end
-                uri_object = JSON.parse(_uri_statement)
-                _jsonld[prop] = []
-                uri_object.each do |uri_object|
-                  _jsonld[prop] << {"@type": uri_object[1], "name": uri_object[2][0], "@id": uri_object[2][1]}
-                end
-              rescue
-                puts "ERROR making JSON-LD parsing property #{prop} statement.cache: #{statement.cache}"
-              end
+              add_anyURI _jsonld, prop, statement.cache
             elsif  statement.source.property.value_datatype == "xsd:dateTime"
-              _dateTime_array = statement.cache
-              if _dateTime_array[0] != ("[" || "{")
-                  _jsonld[prop] = [] << _dateTime_array
-              else
-                _jsonld[prop] = JSON.parse(_dateTime_array)
-              end
+              add_dateTime  _jsonld, prop, statement.cache
             elsif prop == "offer:url"
-              #add offers
-              _jsonld["offers"] = {
-                    "@type": "Offer",
-                    "url": statement.cache,
-                #    "availability": "",
-                #    "price": "",
-                #    "validFrom": ""
-                    "priceCurrency": "CAD" }
+              add_offer _jsonld, "url", statement.cache
+            elsif prop == "offer:price"
+              add_offer _jsonld, "price", statement.cache
+            elsif prop == "CreativeWork:keywords"
+              add_keywords _jsonld, statement.cache
+            elsif prop == "CreativeWork:video"
+              add_video _jsonld, statement.cache
+            elsif prop == "performer:url"
+              add_performer _jsonld, "url", statement.cache
             else
               if prop == "name" || prop == "description"
                 prop = "#{prop}_#{_lang}"
@@ -79,23 +63,6 @@ class StructuredDataController < ApplicationController
           end
         end
       end
-
-      #add location address
-      # location = helpers.get_kg_place _jsonld["location"][:@id]  if _jsonld["location"]
-      # if !location.blank?
-      #   _jsonld["location"]["address"] = {
-      #         "@type": "PostalAddress",
-      #         "streetAddress": location["streetAddress"],
-      #         "addressCountry": location["addressCountry"],
-      #         "addressLocality": location["addressLocality"],
-      #         "addressRegion": location["addressRegion"],
-      #         "postalCode": location["postalCode"]
-      #       }
-      # end
-
-
-      # REPLACE adr: with http://artsdata.ca/resource/
-      _jsonld = eval(_jsonld.to_s.gsub(/adr:/,"http://artsdata.ca/resource/"))
 
       #creates seperate events per startDate each with location is there is a list of locations.
       ## MUST have startDate, location and name
@@ -117,10 +84,20 @@ class StructuredDataController < ApplicationController
 
         # Add Event Series to include all events with the same CreativeWork (Name, description, event page)
         @events << {
-            "@context": "http://schema.org",
+          "@context":
+            {
+              "@vocab": "http://schema.org",
+              "name_fr":{"@id": "name", "@language": "fr"}
+            },
             "@type": "EventSeries",
-             "@id": "#{webpage.rdf_uri.gsub(/adr:/,'http://artsdata.ca/resource/')}"
+             "@id": "#{webpage.rdf_uri}",
+             "location":locations[0],
+             "startDate": dates[0],
+             "name_fr": _jsonld["name_fr"]
             }
+
+        # REPLACE adr: with URI
+        @events = eval(@events.to_s.gsub(/adr:/,"http://laval.footlight.io/resource/"))
 
         render :event_markup, formats: :json
       else
@@ -130,4 +107,101 @@ class StructuredDataController < ApplicationController
 
     end
   end
+
+  private
+    def add_offer jsonld, property, value
+      if !jsonld[:offers]
+        jsonld[:offers] = {
+                        "@type": "Offer",
+                        "url": "",
+                        "availability": "http://schema.org/InStock",
+                        "price": "",
+                        "validFrom": Date.today.to_s(:iso8601),   #Today's date
+                        "priceCurrency": "CAD" }
+      end
+      if property == "url" || property == "price"
+        jsonld[:offers][property] = value
+      else
+          logger.error ("*** Invalid property for schema.org/Offer: #{property} for JSON-LD: #{jsonld.inspect}")
+      end
+      return jsonld
+    end
+
+    def add_keywords jsonld, value
+      #  Event:workPerformed:CreativeWork:keywords
+      if !jsonld[:workPerformed]
+        jsonld[:workPerformed] = {"@type": "CreativeWork"}
+      end
+      jsonld[:workPerformed][:keywords] = value
+      return jsonld
+    end
+
+    def add_video jsonld, value
+     #  Event:workPerformed:CreativeWork:video:VideoObject:url
+      if !jsonld[:workPerformed]
+        jsonld[:workPerformed] = {"@type": "CreativeWork"}
+      end
+
+      if !jsonld[:workPerformed][:video]
+        jsonld[:workPerformed][:video] = {
+            "@type": "VideoObject",
+            "url": []
+          }
+      end
+      jsonld[:workPerformed][:video][:url] << value
+      return jsonld
+    end
+
+    def add_performer jsonld, prop,value
+      #  Event:performer:PerformingGroup:url
+       if !jsonld[:performer]
+         jsonld[:performer] = { "@type": "PerformingGroup" }
+       end
+       jsonld[:performer][prop] = value
+       return jsonld
+    end
+
+    def add_anyURI jsonld, prop, uri_statement
+      begin
+        # Handle 2 possible data structres by making both into a list of arrays.
+        #1: [["source 1","Place",["place name","adr:palce_uri"]],[]]
+        #2: ["source 1","Place",["place name","adr:palce_uri"]]
+        if !uri_statement.starts_with?("[[")
+          uri_statement = "[#{uri_statement}]"
+        end
+        uri_object = JSON.parse(uri_statement)
+        jsonld[prop] = []
+        uri_object.each do |uri_object|
+          jsonld[prop] << {"@type": uri_object[1], "name": uri_object[2][0], "@id": uri_object[2][1]}
+        end
+      rescue
+        puts "ERROR making JSON-LD parsing property #{prop} statement.cache: #{uri_statement}"
+      end
+      return jsonld
+    end
+
+    def add_dateTime   jsonld, prop, dateTime_array
+      if dateTime_array[0] != ("[" || "{")
+          jsonld[prop] = [] << dateTime_array
+      else
+        jsonld[prop] = JSON.parse(dateTime_array)
+      end
+      return jsonld
+    end
+
+    def add_location
+
+            #add location address
+            # location = helpers.get_kg_place _jsonld["location"][:@id]  if _jsonld["location"]
+            # if !location.blank?
+            #   _jsonld["location"]["address"] = {
+            #         "@type": "PostalAddress",
+            #         "streetAddress": location["streetAddress"],
+            #         "addressCountry": location["addressCountry"],
+            #         "addressLocality": location["addressLocality"],
+            #         "addressRegion": location["addressRegion"],
+            #         "postalCode": location["postalCode"]
+            #       }
+            # end
+        end
 end
