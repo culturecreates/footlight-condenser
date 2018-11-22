994e3c7c2b842bc61b7e3b1f1aeb88927fcebafa
Gregory Saumier-Finch <saumierfinch@gmail.com>
2018-11-22 16:24:49 -0500

    added .json format for refresh_rdf_uri

--- app/controllers/statements_controller.rb
+++ app/controllers/statements_controller.rb
@@ -33,7 +33,10 @@ class StatementsController < ApplicationController
     webpages.each do |webpage|
       refresh_webpage_statements(webpage, :force_scrape_every_hrs => params[:force_scrape_every_hrs])
     end
-    redirect_to show_resources_path(rdf_uri: params[:rdf_uri]), notice: 'All statements were successfully refreshed.'
+    respond_to do |format|
+      format.html { redirect_to show_resources_path(rdf_uri: params[:rdf_uri]), notice: 'All statements were successfully refreshed.' }
+      format.json { render json: {message:"URIs refreshed"}.to_json }
+    end
   end
 
   # PATCH /statements/1/refresh
