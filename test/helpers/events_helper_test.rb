16d37ca7ba20a6b2e00960340108e26b686169d6
Gregory Saumier-Finch <gregory@culturecreates.com>
2019-11-04 17:07:16 -0500

    tests for event date sort

---
+++ test/helpers/events_helper_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class EventsHelperTest < ActionView::TestCase
+
+
+  test "Convert Str to first Date" do
+    expected_output = DateTime.parse("Wed, 06 May 2020 19:30:00 -0400")
+    assert_equal expected_output, parse_date_string_array("2020-05-06T19:30:00-04:00")
+  end
+
+
+  test "Convert Stringified Array of dates to first Date" do
+    expected_output = DateTime.parse("Sat, 16 Nov 2019 21:00:00 -0500")
+    assert_equal expected_output, parse_date_string_array("[\"2019-11-16T21:00:00-05:00\", \"2019-11-16T23:30:00-05:00\"]")
+  end
+
+
+end
