62818646270cc67b6abaf58e4a36ff2e7a8461da
Gregory Saumier-Finch <gregory@culturecreates.com>
2020-01-28 20:10:25 -0500

    Added Label field to Sources

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -91,6 +91,6 @@ class SourcesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def source_params
-      params.require(:source).permit(:algorithm_value, :language, :selected, :selected_by, :next_step, :render_js, :property_id, :website_id)
+      params.require(:source).permit(:algorithm_value, :label, :language, :selected, :selected_by, :next_step, :render_js, :property_id, :website_id)
     end
 end
