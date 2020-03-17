4cbc7794ee8e6eb9bcd58d7b9aafb94ed44b280a
Gregory Saumier-Finch <gregory@culturecreates.com>
2020-03-17 13:07:45 -0400

    changed test logic for publishable JSON-LD

--- app/helpers/webpages_helper.rb
+++ app/helpers/webpages_helper.rb
@@ -2,6 +2,6 @@ module WebpagesHelper
     def missing_required_properties event_statement_collection
         #receive a set of statements for an event and check if the event is publishable
         mandatory_schema = ["http://schema.org/name", "http://schema.org/startDate", "http://schema.org/location"]
-        problem_statements = event_statement_collection.select{ |s|  mandatory_schema.include?(s.source.property.uri) && ( s.status != "ok" && s.status  != "updated")  || s.cache == "[]" ||  s.cache.blank? }
+        problem_statements = event_statement_collection.select{ |s|  mandatory_schema.include?(s.source.property.uri) && (( s.status != "ok" && s.status  != "updated")  || s.cache == "[]" ||  s.cache.blank? ) }
     end
 end
