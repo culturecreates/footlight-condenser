310e180a8006077606a09219df38515098158ff4
Gregory Saumier-Finch <gregory@culturecreates.com>
2020-05-06 11:33:56 -0400

    refactored valid dates in model

--- app/models/statement.rb
+++ app/models/statement.rb
@@ -7,7 +7,14 @@ class Statement < ApplicationRecord
   # for pagination
   self.per_page = 100
 
-  STATUSES = { initial: 'Initial', missing: 'Missing', ok: 'Ok', problem: 'Problem',updated: 'Updated' }
+  STATUSES = {
+    initial: 'Initial',
+    missing: 'Missing',
+    ok: 'Ok',
+    problem: 'Problem',
+    updated: 'Updated'
+  }.freeze
+
   validates :status, inclusion: { in: STATUSES.keys.map(&:to_s) }
 
   STATUSES.keys.each do |type|
@@ -16,25 +23,53 @@ class Statement < ApplicationRecord
     self.const_set(type.upcase, type)
   end
 
-  def save
-    if !self.changed_attributes[:cache].nil?
-      self.cache_changed = Time.new
-      unless self.status == "initial" && self.status_origin == "condenser_refresh"
-        # condenser cannot update inself from initial to update state. Need a human to have seen it first.
-        self.status = "updated"
-      end
-      self.status_origin = "condenser_refresh"
+  before_save :set_cache_changed
+  before_save :check_mandatory_properties
+
+  def set_cache_changed
+    return unless self.changed_attributes[:cache].present?
+
+    self.cache_changed = Time.new
+    unless self.status == "initial" && self.status_origin == "condenser_refresh"
+      # condenser cannot update inself from initial to update state. Need a human to have seen it first.
+      self.status = "updated"
     end
-    property_label = self.source.property.label
-    if property_label == "Location"
-      if !self.cache.include?('http') &&  self.status != "problem"
-        self.status = "missing"
-      end
-    elsif property_label == "Dates"
-      if self.cache == "[]" &&  self.status != "problem"
-        self.status = "missing"
-      end
+    self.status_origin = "condenser_refresh"
+  end
+
+  # check for mandatory properties unless status is already a problem
+  def check_mandatory_properties
+    return if status == 'problem'
+
+    property_label = source.property.label
+    if property_label == 'Location'
+      self.status = 'missing' unless cache.include?('http')
+    elsif property_label == 'Dates'
+      self.status = 'missing' unless valid_date?
     end
-    super
+  end
+
+  def valid_date?
+    begin
+      date_array = JSON.parse(cache)
+    rescue JSON::ParserError
+      # convert string to array
+      date_array = Array(cache)
+    end
+    # look for valid dates in array
+    if date_array.select { |d| d if valid_iso_date?(d) }.count > 0
+      true
+    else
+      false
+    end
+  end
+
+  def valid_iso_date?(date_string)
+    begin
+      Date.iso8601(date_string)
+      true
+    rescue ArgumentError
+      false
+    end
   end
 end
--- test/models/statement_test.rb
+++ test/models/statement_test.rb
@@ -1,7 +1,58 @@
 require 'test_helper'
 
 class StatementTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @statement = Statement.new(cache: '2020-05-23T13:30:00-04:00',
+                               source: sources(:twelve))
+  end
+
+  test 'valid: date string' do
+    assert @statement.valid_date?
+  end
+
+  test 'valid: array of dates with one good' do
+    @statement.cache = "[\"2020-05-23T13:30:00-04:00\"]"
+    assert @statement.valid_date?
+  end
+
+  test 'valid: array of dates with one good and one bad' do
+    @statement.cache = "[\"2020-05-23T13:30:00-04:00\", \"error: missing date input\"]"
+    assert @statement.valid_date?
+  end
+
+  test 'valid: array of dates with two good' do
+    @statement.cache = "[\"2020-05-23T13:30:00-04:00\", \"2020-12-23T13:30:00-04:00\"]"
+    assert @statement.valid_date?
+  end
+
+  test 'missing date' do
+    @statement.cache = "[]"
+    assert_not @statement.valid_date?
+  end
+
+  test 'invalid: array of dates with one bad' do
+    @statement.cache = "[\"bad date input\"]"
+    assert_not @statement.valid_date?
+  end
+
+  test 'invalid: array of dates with one error' do
+    @statement.cache = "[\"error: missing date input\"]"
+    assert_not @statement.valid_date?
+  end
+
+  test 'invalid: array of dates with two errors' do
+    @statement.cache = "[\"error: missing date input\", \"error: missing another date input\"]"
+    assert_not @statement.valid_date?
+  end
+
+  # valid_iso_date?
+  test 'valid iso date' do
+    assert @statement.valid_iso_date?('2020-05-23T13:30:00-04:00')
+  end
+
+  test 'invalid iso date' do
+    assert_not @statement.valid_iso_date?('error')
+  end
+
+
 end
