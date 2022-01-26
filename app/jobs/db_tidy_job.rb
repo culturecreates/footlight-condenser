e63ef9d922ce4dfb10c4dcc82ee9d0e59942db09
Gregory Saumier-Finch <gregory@culturecreates.com>
2022-01-26 16:44:38 -0500

    Removed messages older than 1 month

--- app/jobs/db_tidy_job.rb
+++ app/jobs/db_tidy_job.rb
@@ -2,7 +2,7 @@
 class DbTidyJob < ApplicationJob
   queue_as :default
   def perform()
-    Message.where("updated_at < ?", Date.today - 2.months).delete_all  # delete_all does not delete dependancies
+    Message.where("updated_at < ?", Date.today - 1.months).delete_all  # delete_all does not delete dependancies
     # Find websites with lots of Event pages, and delete old webpages based on archive_date
     websites = Webpage.includes(:rdfs_class).where( rdfs_classes: {name: "Event"}).group(:website).count.select { |website,count| count > 100 }.map { |website,count| website}
     Webpage.where(website: websites).where("archive_date < ?", Date.today - 1.months).destroy_all # destroy_all will delete all statements per webpage. Destroys the records by instantiating each record and calling its #destroy method. Each object's callbacks are executed (including :dependent association options).
