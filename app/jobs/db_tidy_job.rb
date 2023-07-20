7e6ebc26e24d1726508f392b4aee9f0b4fba7150
Gregory Saumier-Finch <gregory@culturecreates.com>
2023-07-20 15:42:14 -0400

    Tidy events by deleting only event webpages

--- app/jobs/db_tidy_job.rb
+++ app/jobs/db_tidy_job.rb
@@ -5,6 +5,6 @@ class DbTidyJob < ApplicationJob
     Message.where("updated_at < ?", Date.today - 1.months).delete_all  # delete_all does not delete dependancies
     # Find websites with lots of Event pages, and delete old webpages based on archive_date
     websites = Webpage.includes(:rdfs_class).where( rdfs_classes: {name: "Event"}).group(:website).count.select { |website,count| count > 100 }.map { |website,count| website}
-    Webpage.where(website: websites).where("archive_date < ?", Date.today - 1.months).destroy_all # destroy_all will delete all statements per webpage. Destroys the records by instantiating each record and calling its #destroy method. Each object's callbacks are executed (including :dependent association options).
+    Webpage.includes(:rdfs_class).where(website: websites).where("archive_date < ?", Date.today - 1.months, rdfs_classes: {name: "Event"}).destroy_all # destroy_all will delete all statements per webpage. Destroys the records by instantiating each record and calling its #destroy method. Each object's callbacks are executed (including :dependent association options).
   end
 end
