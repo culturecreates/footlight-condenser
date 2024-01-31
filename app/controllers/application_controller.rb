712c71e116d17cee8cbfbbdf9651e9f64900e2f4
Gregory Saumier-Finch <gregory@culturecreates.com>
2024-01-31 11:49:29 -0500

    added authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 # ApplicationController
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, prepend: true
-  before_action :set_sticky_seedurl
+  before_action :authenticate, :set_sticky_seedurl
 
   private
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == Rails.application.credentials.basic_authentication[:username] && 
+      password == Rails.application.credentials.basic_authentication[:password]
+    end
+  end
 
   def set_sticky_seedurl
     if params[:seedurl].blank?
