42ef54e9f2551b2d9e91db2b465067748c3de8b3
Gregory Saumier-Finch <gregory@culturecreates.com>
2024-02-21 14:23:32 -0500

    skip authentication while running tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 # ApplicationController
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, prepend: true
-  before_action :authenticate, :set_sticky_seedurl
+  before_action :authenticate, unless: -> { Rails.env.test? }
+  before_action :set_sticky_seedurl
 
   private
 
   def authenticate
+    return true if Rails.env.development?
     authenticate_or_request_with_http_basic do |username, password|
       username == Rails.application.credentials.basic_authentication[:username] && 
       password == Rails.application.credentials.basic_authentication[:password]
