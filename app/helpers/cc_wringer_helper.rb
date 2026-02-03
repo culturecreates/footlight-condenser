49653419ccb6a83aac38ca09a489a8e4ded38e58
kmdvs <kim.davis@startmail.com>
2026-02-03 00:20:43 -0500

    Fix: normalize and encode wring URLs consistently

--- app/helpers/cc_wringer_helper.rb
+++ app/helpers/cc_wringer_helper.rb
@@ -1,17 +1,33 @@
-module CcWringerHelper
-  def use_wringer(url, render_js = false, options={})
-    defaults = { :force_scrape_every_hrs => nil }
-    options = defaults.merge(options)
-    url = url.first if url.class == Array
+require "uri"
 
-    escaped_url = CGI.escape(url)
-    path = "/websites/wring?uri=#{escaped_url}&format=raw&include_fragment=true"
-    path += "&use_phantomjs=true" if render_js
-    path += "&force_scrape_every_hrs=#{options[:force_scrape_every_hrs]}" if options[:force_scrape_every_hrs]
-    path += "&json_post=true" if options[:json_post]
-  
-    logger.info("***  calling wringer with: #{get_wringer_url_per_environment() + path} ")
-    return get_wringer_url_per_environment() + path
+module CcWringerHelper
+  def use_wringer(url, render_js = false, options = {})
+    defaults = { force_scrape_every_hrs: nil }
+    options = defaults.merge(options)
+    url = url.first if url.is_a?(Array)
+    url = normalize_url(url)
+
+    query = {
+      uri: url,
+      format: "raw",
+      include_fragment: "true"
+    }
+    query[:use_phantomjs] = "true" if render_js
+    query[:force_scrape_every_hrs] = options[:force_scrape_every_hrs] if options[:force_scrape_every_hrs]
+    query[:json_post] = "true" if options[:json_post]
+
+    path = "/websites/wring?#{URI.encode_www_form(query)}"
+    logger.info("*** calling wringer with: #{get_wringer_url_per_environment}#{path}")
+    "#{get_wringer_url_per_environment}#{path}"
+  end
+
+  def normalize_url(url)
+    u = url.to_s.strip
+    uri = URI.parse(u)
+    uri.fragment = nil
+    uri.to_s
+  rescue URI::InvalidURIError
+    u
   end
 
   def safe_wringer_call
@@ -25,25 +41,35 @@ module CcWringerHelper
   end
 
   def wringer_received_404?(url)
-    escaped_url = CGI.escape(url) # wringer stores the url escaped
-    double_escaped_url = CGI.escape(escaped_url)
-    path = "/websites.json?term=#{double_escaped_url}"
-    data = HTTParty.get(get_wringer_url_per_environment() + path)
-  
-    webpage = data.first 
-    if webpage["http_response_code"] == 404 && webpage["uri"] == escaped_url
-      return true
-    else
-      return false
+    url = url.first if url.is_a?(Array)
+    url = normalize_url(url)
+
+    result = safe_wringer_call do
+      stored_uri = CGI.escape(url)
+
+      path = "/websites.json?#{URI.encode_www_form(term: stored_uri)}"
+      resp = HTTParty.get("#{get_wringer_url_per_environment}#{path}")
+
+      ok = resp.respond_to?(:code) && resp.code.to_i == 200
+      next false unless ok
+
+      data = resp.parsed_response
+      next false unless data.is_a?(Array) && data.first.is_a?(Hash)
+
+      webpage = data.first
+      webpage["http_response_code"].to_i == 404 && webpage["uri"] == stored_uri
     end
+
+    return false if result.is_a?(Hash) && result[:abort_update]
+    
+    !!result
   end
-  
+
   def get_wringer_url_per_environment
-    if Rails.env.development?  || Rails.env.test?
+    if Rails.env.development? || Rails.env.test?
       "http://localhost:3009"
     else
       "http://footlight-wringer.herokuapp.com"
     end
   end
-
 end
