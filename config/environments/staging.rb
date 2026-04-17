11975636d33bd867524e06970c001960d9315b66
kmdvs <kim.davis@startmail.com>
2026-04-17 12:59:19 -0400

    Chore: staging URL

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -91,5 +91,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.wringer_url = ENV.fetch("WRINGER_URL", "https://footlight-wringer-staging.herokuapp.com")
+  config.wringer_url = ENV.fetch("WRINGER_URL", "https://footlight-wringer-staging-a876afe2e40f.herokuapp.com/")
 end
